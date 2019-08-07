dbf0a4574711ed24f8a1ccd75a05e49e06c92e7b
Adel Kamel <adelkamel1982@gmail.com>
2019-08-07 12:45:41 +0200

    Display high scores

--- js/end.js
+++ js/end.js
@@ -3,6 +3,10 @@ const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+const MAX_HIGH_SCORE = 5;
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
@@ -12,5 +16,16 @@ username.addEventListener('keyup', () => {
 saveHighScore = (e) => {
   console.log('Clicked the save btn');
   e.preventDefault();
-  
-}
+
+  const score = {
+    score: Math.floor(Math.random() * 100),
+    name: username.value
+  };
+  highScores.push(score);
+  highScores.sort((a,b) => b.score - a.score);
+  highScores.splice(5);
+
+  localStorage.setItem('highScores', JSON.stringify(highScores));
+  window.location.assign('/');
+
+};
